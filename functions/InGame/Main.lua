57429a334eb55719ec084eca7b0c74c1e7bc96a4
daz <71845855+dazscripts@users.noreply.github.com>
2023-07-22 15:55:28 +0000

    additions

--- functions/InGame/Main.lua
+++ functions/InGame/Main.lua
@@ -3,6 +3,7 @@ Char = plr.Character
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dazscripts/specter/main/functions/GameFunctions.lua"))()
 
 EquipItem("Flashlight")
+task.wait(0.2)
 DropItem("Flashlight")
 
 GetItem("EMF Reader")
@@ -23,15 +24,19 @@ task.wait(0.2)
 fireproximityprompt(WS.Van.Close.VanPrompt)
 
 EquipItem("EMF Reader")
+task.wait(0.2)
 Toggle()
 
 TeleportToGhostRoom()
 
 EquipItem("Spirit Box")
+task.wait(0.2)
 Toggle()
+task.wait(0.2)
 DropItem("Spirit Box")
 
 EquipItem("Book")
+task.wait(0.2)
 DropItem("Book")
 
 for i = 1,3 do
