cf389a0d7d3c27756264c862074483ab5f5eae18
daz <71845855+dazscripts@users.noreply.github.com>
2023-07-24 00:06:46 +0000

    changed main

--- functions/InGame/Main.lua
+++ functions/InGame/Main.lua
@@ -1,13 +1,13 @@
-Char = plr.Character
+    Char = plr.Character
 
-ImportantGUIS = {}
+    ImportantGUIS = {}
 
-for _, v in game:GetService("Players").zed_XESGroupHolder.PlayerGui.Gui.Journal.Content.Evidence.EvidenceCheckboxes:GetChildren() do
-    if v:IsA("Frame") then
-        table.insert(ImportantGUIS, v)
+    for _, v in plr.PlayerGui.Gui.Journal.Content.Evidence.EvidenceCheckboxes:GetChildren() do
+        if v:IsA("Frame") then
+            table.insert(ImportantGUIS, v)
+        end
     end
-end
 
-load("https://raw.githubusercontent.com/dazscripts/specter/main/functions/GameFunctions.lua")
-load("https://raw.githubusercontent.com/dazscripts/specter/main/functions/InGame/Extra/GameBegin.lua")
-load("https://raw.githubusercontent.com/dazscripts/specter/main/functions/Misc/Evidence.lua")
+    load("https://raw.githubusercontent.com/dazscripts/specter/main/functions/GameFunctions.lua")
+    load("https://raw.githubusercontent.com/dazscripts/specter/main/functions/InGame/Extra/GameBegin.lua")
+    load("https://raw.githubusercontent.com/dazscripts/specter/main/functions/Misc/Evidence.lua")
