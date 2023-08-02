652a6ecd2e05d77f7e86cf4837c62c54d2360ed1
daz <71845855+dazscripts@users.noreply.github.com>
2023-08-02 18:16:03 +0000

    config

--- Loader.lua
+++ Loader.lua
@@ -23,11 +23,11 @@ local GameId = 8417221956
 Notification.new("success", "Specter Autofarm V1.2.4 ALPHA", "Made by Drastic", true, 3)
 
 if game.PlaceId == LobbyId then
-    task.wait(5)
+    task.wait(1)
     Notification.new("Info", "Lobby Detected...", "Starting Autofarm", true, 3)
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/dazscripts/specter/main/functions/InGame/Lobby.lua"))()
 elseif game.PlaceId == GameId then
-    task.wait(5)
+    task.wait(10)
     Notification.new("Info", "InGame Detected...", "Starting Autofarm", true, 3)
     EquipmentPath = WS.Equipment
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/dazscripts/specter/main/functions/InGame/Main.lua"))()
